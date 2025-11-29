3cca7f68951dca8e6c786b39f0d4f33406338f83
nagomin <nagominnagonago1995@gmail.com>
2025-11-29 21:25:45 +0900

    ジャンプ機能の追加

--- Main.cpp
+++ Main.cpp
@@ -13,9 +13,21 @@ void Main()
 
 	// プレイヤーの X 座標 | Player's X position
 	double playerPosX = 400;
+	double playerPosY = 540;
+
+	constexpr double gravity = 1500;
+	double jumptime = 0;
+	constexpr double v0 = 1000;
+
+	constexpr double ground = 540;
+
+	bool isPlayerJumping = false;
 
 	// プレイヤーが右を向いているか | Whether player is facing right
 	bool isPlayerFacingRight = true;
+
+	constexpr double leftWall = 60;
+	constexpr double rightWall = 740;
 
 	while (System::Update())
 	{
@@ -24,7 +36,7 @@ void Main()
 		if (KeyLeft.pressed() || KeyA.pressed())
 		{
 			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
+			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), leftWall);
 			isPlayerFacingRight = false;
 		}
 
@@ -32,12 +44,26 @@ void Main()
 		if (KeyRight.pressed() || KeyD.pressed())
 		{
 			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
+			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), rightWall);
 			isPlayerFacingRight = true;
 		}
 
+		if (KeySpace.pressed() || isPlayerJumping)
+		{
+			double y = 0.5*gravity*jumptime*jumptime - v0*jumptime + ground;
+			playerPosY = y;
+			isPlayerJumping = true;
+
+			if (playerPosY >= ground)
+			{
+				isPlayerJumping = false;
+				jumptime = 0;
+			}
+			jumptime = jumptime + Scene::DeltaTime();
+		}
+
 		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
+		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, playerPosY);
 	}
 }
 
